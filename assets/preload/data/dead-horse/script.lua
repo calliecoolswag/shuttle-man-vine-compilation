514fee0ec5fb76bc048797100b9c245eb76b0120
teamsussin <82988956+teamsussin@users.noreply.github.com>
2022-01-18 17:26:29 -0500

    j

---
+++ assets/preload/data/dead-horse/script.lua
@@ -0,0 +1,86 @@
+local xx = 450;
+local yy = 600;
+local xx2 = 955;
+local yy2 = 700;
+local ofs = 45;
+local ofs2 = 45;
+local followchars = true;
+
+function onUpdate()
+		
+	if followchars == true then
+
+        if mustHitSection == false then
+           
+			setProperty('defaultCamZoom', 0.9)
+
+			if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            
+			if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            
+			if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+           
+			if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+		
+        else
+           
+			setProperty('defaultCamZoom', 2)
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs2,yy2)
+            end
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs2,yy2)
+            end
+            
+			if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs2)
+            end
+           
+			if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs2)
+            end
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+		
+        end
+    
+        triggerEvent('Camera Follow Pos','','')
+    
+	end
+
+
+    function onEndSong()
+    
+     if not allowEndShit and isStoryMode and not seenCutscene then
+    
+      setProperty('inCutscene', true);
+    
+      startDialogue('dialogue2'); 
+    
+      allowEndShit = true;
+    
+      return Function_Stop;
+    
+     end
+    
+     return Function_Continue;
+    
+    end
+
+end
